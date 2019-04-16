4fdb7d8d691c229de4be796d102506d2af61696e
wanghongyan <957737702@qq.com>
2019-04-16 16:56:14 +0800

    '初始版本'

---
+++ generator/index.js
@@ -0,0 +1,23 @@
+module.exports = (api, opts, rootOptions) => {
+  api.extendPackage({
+    devDependencies: {
+      "vuepress": "^0.10.0"
+    }
+  });
+
+  api.render({
+    './docs/README.md': './templates/docs/README.md',
+    './docs/.vuepress/config.js': './templates/docs/.vuepress/config.js',
+    './docs/core.md': './templates/docs/core.md',
+    './docs/deploy.md': './templates/docs/deploy.md',
+    './docs/version.md': './templates/docs/version.md'
+  });
+
+  api.extendPackage({
+    scripts: {
+      "docs-dev": "vuepress dev docs",
+      "docs-build": "vuepress build docs",
+    }
+  });
+
+};
---
+++ generator/templates/docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  title: "朋来前端 开发规范&文档积累",
+  description: "前端开发规范, 朋来前端, 朋来互动, 文档资产",
+  dest: 'dist-docs',
+
+  themeConfig: {
+    repo: 'xxx',
+    editLinks: true,
+    docsDir: 'docs',
+    editLinkText: '在 GitHub 上编辑此页',
+    lastUpdated: '上次更新',
+    sidebar: [
+      {
+        title: "项目说明文档",
+        collapsable: false,
+        children: [
+          ['deploy', '部署说明文档'],
+          ['version', '版本更新说明'],
+          ['core', '核心业务说明']
+        ]
+      }
+    ]
+  }
+}
